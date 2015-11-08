b067f1700e8bcc19969ceecd8a979f798896e1f1
James Culbertson <info@turnkey-commerce.com>
2015-11-08 14:47:59 -0600

    Add insert and select for Site.

--- database/database.go
+++ database/database.go
@@ -10,6 +10,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+//Site is the website that will be monitored.
+type Site struct {
+	Name                string
+	IsActive            bool
+	URL                 string
+	PingIntervalSeconds int
+	TimeoutSeconds      int
+}
+
 // InitializeDB creates the DB file and imports the schema.
 func InitializeDB() (*sql.DB, error) {
 	dbPath := "./go-ping-sites.db"
@@ -21,15 +30,55 @@ func InitializeDB() (*sql.DB, error) {
 	}
 
 	dbCreatePath, _ := filepath.Abs("../database/create_database.sql")
-	dat, err := ioutil.ReadFile(dbCreatePath)
+	createStatements, err := ioutil.ReadFile(dbCreatePath)
 	if err != nil {
 		return nil, err
 	}
-	sqlCreate := (string(dat))
+	sqlCreate := (string(createStatements))
+
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
 
 	_, err = db.Exec(sqlCreate)
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
+
+	tx.Commit()
 	return db, nil
+}
+
+//CreateSite inserts a new site in the DB.
+func (s Site) CreateSite(db *sql.DB) (int64, error) {
+	result, err := db.Exec(
+		"INSERT INTO Sites (Name, IsActive, URL, PingIntervalSeconds, TimeoutSeconds) VALUES ($1, $2, $3, $4, $5)",
+		s.Name,
+		s.IsActive,
+		s.URL,
+		s.PingIntervalSeconds,
+		s.TimeoutSeconds,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	siteID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return siteID, nil
+}
+
+//GetSite gets a site from the DB by SiteID.
+func (s *Site) GetSite(db *sql.DB, siteID int64) error {
+	err := db.QueryRow("SELECT Name, IsActive, URL, PingIntervalSeconds, TimeoutSeconds FROM Sites WHERE SiteID = $1", siteID).
+		Scan(&s.Name, &s.IsActive, &s.URL, &s.PingIntervalSeconds, &s.TimeoutSeconds)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- database/database_test.go
+++ database/database_test.go
@@ -1,10 +1,16 @@
-package database
+package database_test
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/turnkey-commerce/go-ping-sites/database"
+)
 
 // TestCreateDb tests the creation of the database.
 func TestCreateDb(t *testing.T) {
-	db, err := InitializeDB()
+	db, err := database.InitializeDB()
+	defer db.Close()
 	if err != nil {
 		t.Fatal("Failed to create database:", err)
 	}
@@ -12,5 +18,35 @@ func TestCreateDb(t *testing.T) {
 	errPing := db.Ping()
 	if errPing != nil {
 		t.Fatal("Failed to ping database:", errPing)
+	}
+}
+
+// TestCreateNewSite tests adding a new site in the database and then retrieving it.
+func TestCreateAndGetNewSite(t *testing.T) {
+	db, err := database.InitializeDB()
+	defer db.Close()
+	if err != nil {
+		t.Fatal("Failed to create database:", err)
+	}
+
+	s := database.Site{Name: "Test", IsActive: true, URL: "http://www.google.com", PingIntervalSeconds: 60, TimeoutSeconds: 30}
+	siteID, errCreate := s.CreateSite(db)
+	if errCreate != nil {
+		t.Fatal("Failed to create new site:", errCreate)
+	}
+
+	// Autonumber should be 1 on the first create.
+	if siteID != 1 {
+		t.Fatal("Expected 1, got ", siteID)
+	}
+
+	//Check that the saved site is as Expected
+	saved := database.Site{}
+	err = saved.GetSite(db, siteID)
+	if err != nil {
+		t.Fatal("Failed to retrieve new site:", err)
+	}
+	if !reflect.DeepEqual(s, saved) {
+		t.Fatal("New site saved not equal to input:\n", saved, s)
 	}
 }
