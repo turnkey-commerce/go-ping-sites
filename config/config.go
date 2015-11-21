601c8502d9e0cee454fd7db71abd1c3fd6b78148
turnkey-commerce <info@turnkey-commerce.com>
2015-11-20 23:30:33 -0600

    Implement Config, SMTP, and SMS.

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+var configFile = "config.toml"
+
+// Settings stores the configuration for the SMTP email and Twilio SMS accounts.
+var Settings struct {
+	SMTP struct {
+		EmailAddress string
+		Password     string
+		Server       string
+		Port         string
+	}
+	Twilio struct {
+		AccountSid string
+		AuthToken  string
+		Number     string
+	}
+}
+
+func init() {
+	if _, err := toml.DecodeFile(configFile, &Settings); err != nil {
+		log.Fatal(err)
+	}
+}
--- notifier/notifier.go
+++ notifier/notifier.go
@@ -2,8 +2,12 @@ package notifier
 
 import (
 	"log"
+	"net/smtp"
 	"sync"
 
+	"github.com/sfreiberg/gotwilio"
+
+	"github.com/turnkey-commerce/go-ping-sites/config"
 	"github.com/turnkey-commerce/go-ping-sites/database"
 )
 
@@ -37,6 +41,48 @@ func (n *Notifier) Notify() {
 }
 
 func send(c database.Contact, message string, subject string, wg *sync.WaitGroup) {
+	var err error
 	log.Println("Sending notifications for", c.Name, subject, message)
+	if c.EmailActive && len(c.EmailAddress) > 0 {
+		err = sendEmail(c.EmailAddress, message, subject)
+		if err != nil {
+			log.Println("Error sending email:", err)
+		}
+	}
+
+	if c.SmsActive && len(c.SmsNumber) > 0 {
+		err = sendSms(c.SmsNumber, message)
+		if err != nil {
+			log.Println("Error sending SMS:", err)
+		}
+	}
+
 	wg.Done()
+}
+
+func sendEmail(recipient string, message string, subject string) error {
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", config.Settings.SMTP.EmailAddress, config.Settings.SMTP.Password,
+		config.Settings.SMTP.Server)
+	server := config.Settings.SMTP.Server + ":" + config.Settings.SMTP.Port
+	to := []string{recipient}
+	from := "sender@example.org"
+	msg := []byte("Subject: " + subject + "\r\n\r\n" +
+		message + "\r\n")
+	err := smtp.SendMail(server, auth, from, to, msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func sendSms(smsNumber string, message string) error {
+	twilio := gotwilio.NewTwilioClient(config.Settings.Twilio.AccountSid, config.Settings.Twilio.AuthToken)
+	from := config.Settings.Twilio.Number
+
+	_, _, err := twilio.SendSMS(from, smsNumber, message, "", "")
+	if err != nil {
+		return err
+	}
+	return nil
 }
