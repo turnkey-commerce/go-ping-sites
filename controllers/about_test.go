b66432ed728c2cffc1a1a70f6089dbc030aa86f2
James Culbertson <info@turnkey-commerce.com>
2016-01-19 17:39:03 -0600

    Update MockCurrentUserGetter to pass properties to mock methods.

--- controllers/about_test.go
+++ controllers/about_test.go
@@ -9,7 +9,9 @@ import (
 
 func TestAboutController(t *testing.T) {
 
-	mockUserGetter := MockCurrentUserGetter{}
+	mockUserGetter := MockCurrentUserGetter{Username: "jules",
+		FlashMessages: []string{"Log in to do that."}}
+
 	req, err := http.NewRequest("GET", "http://example.com/about", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -24,11 +26,21 @@ func TestAboutController(t *testing.T) {
 	w := httptest.NewRecorder()
 	ac.get(w, req)
 
-	if !strings.Contains(w.Body.String(), "Logged in as test") {
+	body := w.Body.String()
+
+	if !strings.Contains(body, "Logged in as jules") {
 		t.Error("Current user not rendered properly.")
 	}
 
-	if !strings.Contains(w.Body.String(), `<li class="active"><a href="/about">About</a></li>`) {
+	if !strings.Contains(body, `<li class="active"><a href="/about">About</a></li>`) {
 		t.Error("Navigation active tab not rendered as expected.")
+	}
+
+	if !strings.Contains(body, `<div class="alert alert-danger" role="alert">Log in to do that.</div>`) {
+		t.Error("Flash message not rendered as expected.")
+	}
+
+	if !strings.Contains(body, `<title>Go Ping Sites - About</title>`) {
+		t.Error("Flash message not rendered as expected.")
 	}
 }
--- controllers/user_mocks.go
+++ controllers/user_mocks.go
@@ -6,15 +6,22 @@ import (
 	"github.com/apexskier/httpauth"
 )
 
+// MockCurrentUserGetter provides the base struct for the methods to mock the
+// httpauth package. The properties on this struct can vary the results returned.
 type MockCurrentUserGetter struct {
+	FlashMessages []string
+	Username      string
+	UserError     error
 }
 
+// Messages is the mock of the Messasges method from the httpauth package.
 func (m MockCurrentUserGetter) Messages(rw http.ResponseWriter, req *http.Request) []string {
-	var messages []string
+	messages := m.FlashMessages
 	return messages
 }
 
+// CurrentUser is the mock of the CurrentUser method from the httpauth package.
 func (m MockCurrentUserGetter) CurrentUser(rw http.ResponseWriter, req *http.Request) (user httpauth.UserData, e error) {
-	user = httpauth.UserData{Username: "test"}
-	return user, nil
+	user = httpauth.UserData{Username: m.Username}
+	return user, m.UserError
 }
